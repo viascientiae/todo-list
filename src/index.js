b788e0c461d2d174d188898ffd528b1d0eecd223
Via Scientiae <144046360+viascientiae@users.noreply.github.com>
2025-08-23 10:06:08 +0000

    Add functionality to add new Todo item

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const newTodoItemButton = document.createElement("button");
 
 newTodoItemButton.textContent = "New Todo";
 
- main.appendChild(newTodoItemButton);
+main.appendChild(newTodoItemButton);
 
 const newTodoItemDialog = document.createElement("dialog");
 
@@ -12,7 +12,7 @@ newTodoItemDialog.innerHTML =  `
   <h2>Todo Details</h2>
   <form>
     <label for="title">Title: <input type="text" name="title" id="title"></label>
-    <label for="description">Description: <input type="text" name="description id="description></label>
+    <label for="description">Description: <input type="text" name="description" id="description"></label>
     <label for="dueDate">Due Date: <input type="date" name="dueDate" id="dueDate"></label>
     <label for="priority">Priority: <input type="number" name="priority" id="priority"></label>
     <button id="submit-todo-item">Submit</button>
@@ -25,3 +25,39 @@ main.appendChild(newTodoItemDialog);
 newTodoItemButton.addEventListener("click", function() {
   newTodoItemDialog.show();
 })
+
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const dueDate = document.getElementById("dueDate");
+const priority = document.getElementById("priority");
+const btnSubmitTodoItem = document.getElementById("submit-todo-item");
+const btnCloseNewTodoItemDialog = document.getElementById("close-dialog");
+
+btnSubmitTodoItem.addEventListener("click", function(e) {
+  e.preventDefault();
+  const titleValue = title.value;
+  const descriptionValue = description.value;
+  const dueDateValue = dueDate.value;
+  const priorityValue = priority.value;
+  addTodoItem(titleValue, descriptionValue, dueDateValue, priorityValue);
+});
+
+let defaultTodoList = [];
+
+localStorage.setItem("defaultTodoList", JSON.stringify(defaultTodoList));
+
+function addTodoItem(title, description, dueDate, priority) {
+  const newTodoItem = new TodoItem(title, description, dueDate, priority);
+  defaultTodoList = JSON.parse(localStorage.getItem("defaultTodoList"));
+  defaultTodoList.push(newTodoItem);
+  localStorage.setItem("defaultTodoList", JSON.stringify(defaultTodoList));
+};
+
+class TodoItem {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+};
