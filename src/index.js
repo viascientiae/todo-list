c0408bb02c7f5a2fa924a8bcbcb78cd02443a907
Via Scientiae <144046360+viascientiae@users.noreply.github.com>
2025-08-23 06:38:21 +0000

    Add dialog and form for new todo item

--- src/index.js
+++ src/index.js
@@ -6,3 +6,24 @@ newTodoItemButton.textContent = "New Todo";
 document.addEventListener("DOMContentLoaded", function() {
  main.appendChild(newTodoItemButton);
 });
+
+const newTodoItemDialog = document.createElement("dialog");
+newTodoItemDialog.innerHTML =  `
+  <h2>Todo Details</h2>
+  <form>
+    <label for="title">Title: <input type="text" name="title" id="title"></label>
+    <label for="description">Description: <input type="text" name="description id="description></label>
+    <label for="dueDate">Due Date: <input type="date" name="dueDate" id="dueDate"></label>
+    <label for="priority">Priority: <input type="number" name="priority" id="priority"></label>
+    <button id="submit-todo-item">Submit</button>
+    <button id="close-dialog">Close</button>
+  </form>
+  `;
+
+document.addEventListener("DOMContentLoaded", function() {
+  main.appendChild(newTodoItemDialog);
+});
+
+newTodoItemButton.addEventListener("click", function() {
+  newTodoItemDialog.show();
+})
