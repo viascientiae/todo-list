51cd956e63744ba3cdbeff3e9ee8b0102ad21778
Via Scientiae <144046360+viascientiae@users.noreply.github.com>
2025-09-01 10:42:13 +0000

    Refactor index.js to use imported modules

--- src/index.js
+++ src/index.js
@@ -1,45 +1,29 @@
-import TodoItem from "./ToDoItem";
-import { loadLists, saveLists } from "./storage";
+import { loadLists } from "./storage";
 import { createNewTodoDialog, updateDOM } from "./dom";
+import { setupEventListeners } from "./events";
 
 const main  = document.querySelector("main");
+const toDoLists = loadLists();
 
-const title = document.getElementById("title");
-const description = document.getElementById("description");
-const dueDate = document.getElementById("dueDate");
-const priority = document.getElementById("priority");
-const list = document.getElementById("list");
-const btnSubmitTodoItem = document.getElementById("submit-todo-item");
-const btnCloseNewTodoItemDialog = document.getElementById("close-dialog");
-btnSubmitTodoItem.addEventListener("click", function(e) {
-  e.preventDefault();
-  const titleValue = title.value;
-  const descriptionValue = description.value;
-  const dueDateValue = dueDate.value;
-  const priorityValue = priority.value;
-  const listValue = list.value;
-  addTodoItem(titleValue, descriptionValue, dueDateValue, priorityValue, listValue);
-  newTodoItemDialog.close();
-  updateDOM();
-});
-btnCloseNewTodoItemDialog.addEventListener("click", function(e) {
-  e.preventDefault();
-  newTodoItemDialog.close();
-});
+const newTodoItemButton = document.createElement("button");
+newTodoItemButton.textContent = "New Todo";
+main.appendChild(newTodoItemButton);
 
-function addTodoItem(title, description, dueDate, priority, list) {
-  const newTodoItem = new TodoItem(title, description, dueDate, priority);
-  let listName = list.toLowerCase();
-  console.log(newTodoItem);
-  console.log(listName);
-  let listFound = toDoLists.find(list => list.name === listName);
-  console.log(listFound);
-  if (listFound) {
-    listFound.items.push(newTodoItem);
-  }
-  else {
-    toDoLists.push({name: listName, items: [newTodoItem]});
-  }
-  localStorage.setItem("toDoLists", JSON.stringify(toDoLists));
-  console.log(JSON.parse(localStorage.getItem("toDoLists")));
-};
+const newTodoItemDialog = createNewTodoDialog();
+main.appendChild(newTodoItemDialog);
+
+newTodoItemButton.addEventListener("click", () => newTodoItemDialog.show());
+
+const selectToDoListContainer = document.createElement("section");
+selectToDoListContainer.innerHTML = `
+  <label for="selectToDoList">Choose a ToDo List to Display</label>
+  <select name="selectTodoList" id="select-todo-list"></select>  
+`;
+main.appendChild(selectToDoListContainer);
+
+const selectToDoList = document.getElementById("select-todo-list");
+const listDisplay = document.createElement("section");
+main.appendChild(listDisplay);
+
+updateDOM(toDoLists, "default", selectToDoList, listDisplay);
+setupEventListeners(main, toDoLists, newTodoItemDialog, selectToDoList, listDisplay, updateDOM);
